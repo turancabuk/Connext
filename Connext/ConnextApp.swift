c193f566c4e7d5b00bd99dc1c2729cd3da10ed1e
turan cabuk <turan6677@gmail.com>
2024-08-04 22:47:50 +0300

    Initial Commit

---
+++ Connext/ConnextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnextApp.swift
+//  Connext
+//
+//  Created by Turan Çabuk on 4.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ConnextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connext/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Connext
+//
+//  Created by Turan Çabuk on 4.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
