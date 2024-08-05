38c7b2a6bb38f190b2e081375344c0f1aa07658b
turan cabuk <turan6677@gmail.com>
2024-08-05 16:05:15 +0300

    LocationsListView has been created.

--- Connext/LocationsListView.swift
+++ Connext/LocationsListView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct LocationsListView: View {
     var body: some View {
-        Text("Location List View")
+        NavigationView{
+            List{
+                ForEach(0..<10) { _ in
+                    HStack{
+                        Image("person")
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                            .cornerRadius(.infinity)
+                            .padding(.vertical, 8)
+                        Spacer()
+                        VStack(alignment: .leading){
+                            Text("Sample Title")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+                            HStack(spacing: 4) {
+                                ForEach(0..<4) { _ in
+                                    Image("person")
+                                        .resizable()
+                                        .frame(width: 48, height: 48)
+                                        .cornerRadius(.infinity)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Grub Spots")
+        }
     }
 }
 
